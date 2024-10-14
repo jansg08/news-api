5ebc85fc9a88913e6d5442bb44f17ee3242abbc1
Jack Hansjee <jack.hansjee@me.com>
2024-10-14 12:25:03 +0100

    Add and test happy path functionality for GET /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const db = require("../db/connection");
 const data = require("../db/data/test-data/");
 const seed = require("../db/seeds/seed");
 const app = require("../app");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -20,6 +21,17 @@ describe("/api/topics", () => {
             expect(typeof slug).toBe("string");
           });
         });
+    });
+  });
+});
+
+describe("/api", () => {
+  describe("GET", () => {
+    test("200: responds with json of all endpoints available", () => {
+      return request(app)
+        .get("/api")
+        .expect(200)
+        .then(({ body }) => expect(body.endpoints).toEqual(endpoints));
     });
   });
 });
---
+++ models/endpoints.model.js
@@ -0,0 +1,5 @@
+const { readFile } = require("fs/promises");
+
+exports.readEndpoints = () => {
+  return readFile(`${__dirname}/../endpoints.json`, "utf-8").then(JSON.parse);
+};
