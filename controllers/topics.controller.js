e2bb4859e77eae56fe0b395e84741a05d60bc663
Jack Hansjee <jack.hansjee@me.com>
2024-10-14 12:19:18 +0100

    Add catch block to topics controller

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,5 +1,7 @@
 const { selectTopics } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
-  return selectTopics().then((topics) => res.status(200).send({ topics }));
+  return selectTopics()
+    .then((topics) => res.status(200).send({ topics }))
+    .catch((err) => next(err));
 };
