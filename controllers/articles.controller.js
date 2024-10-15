81d51f2469ae57f7f8b213c7b60f316c9cf93405
Jack Hansjee <jack.hansjee@me.com>
2024-10-15 14:12:33 +0100

    Add comment_count to response object for GET /api/articles/:article_id

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -7,7 +7,8 @@ const {
 } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
-  return selectArticleById(req.params.article_id)
+  const { article_id } = req.params;
+  return selectArticleById(article_id)
     .then((article) => res.status(200).send({ article }))
     .catch((err) => next(err));
 };
