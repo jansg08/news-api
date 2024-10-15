20a257612fd7bb1829304d51e4cdec5aa3dec12a
Jack Hansjee <jack.hansjee@me.com>
2024-10-15 12:51:27 +0100

    Add catch block to getUsers controller to handle any server errors

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,7 @@
 const { selectUsers } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
-  return selectUsers().then((users) => res.status(200).send({ users }));
+  return selectUsers()
+    .then((users) => res.status(200).send({ users }))
+    .catch((err) => next(err));
 };
