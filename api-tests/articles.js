d5a9cc18b0a929275de0a70627e6d86c8e4a38fe
Jack Hansjee <jack.hansjee@me.com>
2024-10-18 23:39:19 +0100

    Revert test file back to previous state so tests now pass

--- api-tests/articles.js
+++ api-tests/articles.js
@@ -516,7 +516,7 @@ exports.articlesTest = () =>
               username: "icellusedkars",
             })
             .expect(404)
-            .then(({ body }) => expect(body.msg).toBe("found"));
+            .then(({ body }) => expect(body.msg).toBe("Not found"));
         });
         test("400: responds with 'Bad request' when provided with an invalid id", () => {
           return request(app)
